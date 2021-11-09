0eb9d7aa94181e841ae27c30c0d6a0955ab834f4
cnh12 <zetta_byte@naver.com>
2021-11-09 21:26:49 +0900

    211109 자식앱에 calllog 추가

--- app/src/main/java/com/postit/mymomsweather/MainActivity.java
+++ app/src/main/java/com/postit/mymomsweather/MainActivity.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.postit.mymomsweather.databinding.ActivityMainBinding;
+
+import java.nio.file.Files;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -26,6 +30,15 @@ public class MainActivity extends AppCompatActivity {
         binding.showParentButton.setOnClickListener((v)->{
             Intent profileIntent = new Intent(this, ParentListActivity.class);
             startActivity(profileIntent);
+        });
+
+        Button btn_calllog = (Button) findViewById(R.id.btn_calllog);
+        btn_calllog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent calllogIntent = new Intent(getApplicationContext(), CallLogActivity.class);
+                startActivity(calllogIntent);
+            }
         });
 
 
