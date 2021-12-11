0b49305ca0d77c5f20e9a17da7858b8ee14794f5
cnh12 <zetta_byte@naver.com>
2021-12-11 10:58:05 +0900

    211211 자식앱 pieChart 색깔수정

--- app/src/main/java/com/postit/mymomsweather/MainActivity.java
+++ app/src/main/java/com/postit/mymomsweather/MainActivity.java
@@ -48,6 +48,16 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
+        binding.layoutGocommunity.setOnClickListener((v)->{
+            Intent intent = new Intent(this,CommunityActivity.class);
+            startActivity(intent);
+        });
+
+        binding.layoutGogame.setOnClickListener((v)->{
+            Intent intent = new Intent(this,GameActivity.class);
+            startActivity(intent);
+        });
+
         checkPermission();
         initViewPager();
         initParentRecyclerView();
